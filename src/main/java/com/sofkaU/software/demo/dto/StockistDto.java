935f4245e7d2671ddeac4941cf12b7a3d104368d
Marcos Nino <tmnino@uninorte.edu.co>
2022-06-08 16:21:30 -0500

    StockistDto
    StockistDto class was created

---
+++ src/main/java/com/sofkaU/software/demo/dto/StockistDto.java
@@ -0,0 +1,21 @@
+package com.sofkaU.software.demo.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+public class StockistDto {
+
+    private String stockistId;
+
+    @NotBlank(message = "Stockist name cannot be empty")
+    private String stockistName;
+
+    @NotBlank(message = "Stockist personal ID cannot be blank")
+    private String stockistPersonalId;
+
+    @NotBlank(message = "Phone number cannot be blank")
+    private String phoneNumber;
+
+}
