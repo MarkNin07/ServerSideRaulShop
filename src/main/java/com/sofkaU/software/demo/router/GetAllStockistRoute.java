ffc9823205a50070f074147140fb70595116c0fb
Marcos Nino <tmnino@uninorte.edu.co>
2022-06-08 20:27:59 -0500

    GetAllStockistRoute was created

--- src/main/java/com/sofkaU/software/demo/router/GetAllStockistRoute.java
+++ src/main/java/com/sofkaU/software/demo/router/GetAllStockistRoute.java
@@ -1,5 +1,27 @@
 package com.sofkaU.software.demo.router;
 
 
+import com.sofkaU.software.demo.dto.StockistDto;
+import com.sofkaU.software.demo.usecases.GetAllStockistsUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class GetAllStockistRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> AllStockist(GetAllStockistsUseCase allStockists){
+        return route(GET("/get/all/categories"), request -> ServerResponse.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromProducer(allStockists.getAllStockists(), StockistDto.class)));
+    }
+
 }
